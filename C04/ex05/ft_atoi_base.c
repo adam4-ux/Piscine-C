ec328da3a5fde871fea037c59d5be2a4861a647e
adam4-ux <adameddoukkali20063@gmail.com>
2026-07-13 13:26:36 +0100

    1337

---
+++ C04/ex05/ft_atoi_base.c
@@ -0,0 +1,95 @@
+#include <stdio.h>
+int ft_strlen(char *s)
+{
+    int i = 0;
+    while (s[i])
+    {
+        i++;
+    }
+    return i;
+}
+
+int invalid_base(char *base)
+{
+    int i = 0;
+    int j;
+    if(base[0] == '\0' || base[1] == '\0')
+        return 0;
+    while(base[i])
+    {
+        if(base[i] == '+' || base[i] == '-')
+        {
+            return 0;
+        }
+            j = i + 1;
+            while(base[j])
+            {
+                if(base[i] == base[j])
+                {
+                    return 0;
+                }
+                j++;
+            }
+        i++;
+    }
+    return 1;
+}
+
+int indexof_base(char str,char *base)
+{
+    int i = 0;
+    while(base[i])
+    {
+        if(str == base[i])
+        {
+            return i;
+        }
+        i++;
+    }
+    return 0;
+}
+
+int check_base(char str,char *base)
+{
+    int i = 0;
+    while(base[i])
+    {
+        if(str == base[i])
+        {
+            return 1;
+        }
+        i++;
+    }
+    return 0;
+}
+
+int ft_atoi_base(char *str, char *base)
+{
+    int i = 0;
+    int sign = 1;
+    int result = 0;
+    int len_base = ft_strlen(base);
+
+    if(!(invalid_base(base)))
+        return 0;
+
+    while(str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
+        i++;
+
+    while (str[i] == '+' || str[i] == '-')
+    {
+        if(str[i] == '-')
+        {
+            sign *= -1;
+        }
+        i++;
+    }
+
+    while(check_base(str[i],base))
+    {
+        result = result * len_base + indexof_base(str[i],base);
+        i++;
+    }
+
+    return result*sign;
+}
