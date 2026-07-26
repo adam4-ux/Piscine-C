a4d89639d16eefc70d07cb864c81443c93ee79be
adam4-ux <adameddoukkali20063@gmail.com>
2026-07-26 11:19:52 +0100

    occ_z/

---
+++ Exams/Exam01/Level03/occ_z/occ_z.c
@@ -0,0 +1,14 @@
+int occ_z(char *str)
+{
+    int i = 0;
+    int count = 0;
+    while(str[i])
+    {
+        if(str[i] == 'Z')
+        {
+            count++;
+        }
+        i++;
+    }
+    return count;
+}
